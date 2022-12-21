4caf83a72a1d31af0814f2fef1324c69f2955b11
Jesus-mk <118129150+JHerasP@users.noreply.github.com>
2022-12-21 17:53:34 +0100

    Update utiliy-types.ts

--- ts-utility-types/utiliy-types.ts
+++ ts-utility-types/utiliy-types.ts
@@ -29,9 +29,17 @@ export type ReplaceTypes<T, F extends keyof T, R> = Omit<T, F> & Record<F, R>;
 
 /**
 Remplace key name and type
-@param T Intefaz a modificar
+@param T Type
 @param F Keys of T
 @param S New keys
 @param R Type of new keys
 */
 export type ReplaceKeys<T, F extends keyof T, S extends string, R> = Omit<T, F> & Record<S, R>;
+
+/**
+ * Modifies all the keys to be optional exepct for the given keys.
+ * @param T Type
+ * @param F Keys to keep required
+ */
+export type OptionalExceptFor<T, F extends keyof T = keyof T> = Partial<Pick<T, Exclude<keyof T, F>>> &
+  Required<Pick<T, F>>;
